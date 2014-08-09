cd07ed7dacc39b9ed22c0be0bb8968e24ab47975
Prof9 <prof9@outlook.com>
2014-08-09 22:27:03 +0200

    Added option to ItemProvider to randomize chip codes only.

--- src/mmbn/ItemProvider.java
+++ src/mmbn/ItemProvider.java
@@ -8,40 +8,50 @@ import rand.RandomizerContext;
 public class ItemProvider extends DataProvider<Item> {
     private final ChipLibrary chipLibrary;
     
+    private boolean codeOnly;
+    
     public ItemProvider(RandomizerContext context, ItemProducer producer) {
         super(context, producer);
         this.chipLibrary = producer.chipLibrary();
+        this.codeOnly = false;
+    }
+    
+    public void setCodeOnly(boolean codeOnly) {
+        this.codeOnly = codeOnly;
     }
     
     @Override
     protected void randomizeData(Random rng, Item item, int position) {
         if (item.isChip()) {
-            BattleChip oldChip = item.getChip();
+            BattleChip chip = item.getChip();
+            int r;
             
-            // Choose a random new battle chip with the same library and rarity.
-            List<BattleChip> possibleChips = this.chipLibrary.query(
-                    (byte)-1,
-                    oldChip.getRarity(), oldChip.getRarity(),
-                    null,
-                    oldChip.getLibrary(),
-                    0, 99
-            );
-            if (possibleChips.size() <= 0) {
-                possibleChips.add(oldChip);
+            if (!codeOnly) {
+                // Choose a random new battle chip with the same library and rarity.
+                List<BattleChip> possibleChips = this.chipLibrary.query(
+                        (byte)-1,
+                        chip.getRarity(), chip.getRarity(),
+                        null,
+                        chip.getLibrary(),
+                        0, 99
+                );
+                if (possibleChips.size() <= 0) {
+                    possibleChips.add(chip);
+                }
+
+                r = rng.nextInt(possibleChips.size());
+                chip = possibleChips.get(r);
             }
             
-            int r = rng.nextInt(possibleChips.size());
-            BattleChip newChip = possibleChips.get(r);
-            
             // Choose a random code.
-            byte[] possibleCodes = newChip.getCodes();
+            byte[] possibleCodes = chip.getCodes();
             r = rng.nextInt(possibleCodes.length);
             byte newCode = possibleCodes[r];
             
             if (item.type == Item.Type.BATTLECHIP_TRAP) {
-                item.setChipCodeTrap(newChip, newCode);
+                item.setChipCodeTrap(chip, newCode);
             } else {
-                item.setChipCode(newChip, newCode);
+                item.setChipCode(chip, newCode);
             }
         }
     }
