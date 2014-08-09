1ae6708de42e82073f317774140fde11b1c69d9f
Prof9 <prof9@outlook.com>
2014-08-09 22:26:04 +0200

    Added way to clone ShopItem and replace all properties inherited from Item.

--- src/mmbn/ShopItem.java
+++ src/mmbn/ShopItem.java
@@ -9,6 +9,18 @@ public class ShopItem extends Item {
         this.stock = 0;
         this.price = 0;
     }
+    public ShopItem(ShopItem shopItem) {
+        super(shopItem);
+        this.stock = shopItem.stock;
+        this.price = shopItem.price;
+    }
+    
+    public void setItem(Item item) {
+        this.type = item.type;
+        this.value = item.value;
+        this.subValue = item.subValue;
+        this.chip = item.chip;
+    }
     
     public int getStock() {
         return this.stock;
