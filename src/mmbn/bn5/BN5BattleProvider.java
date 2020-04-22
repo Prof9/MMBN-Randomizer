6152c7c9c31bdcc5db8a28dec5a5e3435421fbab
Prof9 <prof9@outlook.com>
2020-04-22 20:00:10 +0200

    Equalize WindBox and VacuumFan ranks, remove Fire-only Kettles

--- src/mmbn/bn5/BN5BattleProvider.java
+++ src/mmbn/bn5/BN5BattleProvider.java
@@ -8,6 +8,39 @@ import rand.DataProvider;
 import rand.RandomizerContext;
 
 public class BN5BattleProvider extends DataProvider<Battle> {
+	private static final byte[][] ENEMY_LEVELS = {
+		{ 1, 2, 3, 4, 5, 6 }, // Mettaur
+		{ 1, 2, 3, 4, 5, 6 }, // Powie
+		{ 1, 2, 3, 4, 5, 6 }, // BugTank
+		{ 1, 2, 3, 4, 5, 6 }, // Handi
+		{ 1, 2, 3, 4, 5, 6 }, // Dominerd
+		{ 1, 2, 3, 4, 5, 6 }, // MetFire
+		{ 1, 2, 3, 4, 5, 6 }, // Shakey
+		{ 1, 2, 3, 4, 5, 6 }, // Trumpy
+		{ 1, 2, 3, 4, 5, 6 }, // NinJoy
+		{ 1, 1, 3, 3, 5, 5 }, // WindBox
+		{ 1, 2, 3, 4, 5, 6 }, // Lark
+		{ 1, 2, 3, 4, 5, 6 }, // BomBoy
+		{ 1, 2, 3, 4, 5, 6 }, // Zomon
+		{ 1, 2, 3, 4, 5, 6 }, // CannonGuard
+		{ 1, 2, 3, 4, 5, 6 }, // Catack
+		{ 1, 2, 3, 4, 5, 6 }, // Drixol
+		{ 1, 2, 3, 4, 5, 6 }, // Batty
+		{ 1, 2, 3, 4, 5, 6 }, // Skarab
+		{ 1, 2, 3, 4, 5, 6 }, // Champy
+		{ 1, 2, 3, 4, 5, 6 }, // Draggin
+		{ 1, 2, 3, 4, 5, 6 }, // Whirly
+		{ 1, 2, 3, 4, 5, 6 }, // Marina
+		{ 1, 2, 3, 4, 5, 6 }, // Flashy
+		{ 1, 2, 3, 4, 5, 6 }, // Eleogre
+		{ 1, 2, 3, 4, 5, 6 }, // Cactikil
+		{ 1, 2, 3, 4, 5, 6 }, // WuNote
+		{ 1, 2, 3, 4, 5, 6 }, // Appley
+		{ 1, 2, 3, 4, 5, 6 }, // BigBrute
+		{ 1, 2, 3, 4, 5, 6 }, // TinHawk
+		{ 1, 2, 3, 4, 5, 6 }, // Bladia
+	};
+	
 	public BN5BattleProvider(RandomizerContext context, BattleProducer producer) {
 		super(context, producer);
 	}
@@ -24,9 +57,30 @@ public class BN5BattleProvider extends DataProvider<Battle> {
 				if (objValue >= 1 && objValue <= 0xA2) {
 					int enemyFamily = (objValue - 1) / 6;
 					int enemyRank = (objValue - 1) % 6;
-
-					// Choose a random family but keep the same rank.
+					enemyRank = Math.abs(ENEMY_LEVELS[enemyFamily][enemyRank]);
+					
+					// Choose a random family.
 					enemyFamily = rng.nextInt(27);
+					
+					// Choose a random rank of the same difficulty.
+					byte[] possibleRanks = new byte[6];
+					int possibleCount = 0;
+					do {
+						for (byte j = 0; j < 6; j++) {
+							byte possibleRank = ENEMY_LEVELS[enemyFamily][j];
+							if (possibleRank == enemyRank) {
+								possibleRanks[possibleCount++] = j;
+							}
+						}
+						// Reduce enemy rank if not found.
+					} while (possibleCount == 0 && --enemyRank >= 1);
+					if (possibleCount == 0) {
+						// Should never happen!!!
+						continue;
+					}
+					
+					// Choose enemy level.
+					enemyRank = possibleRanks[rng.nextInt(possibleCount)];
 
 					objValue = enemyFamily * 6 + enemyRank + 1;
 				}
--- src/mmbn/bn6/BN6BattleProvider.java
+++ src/mmbn/bn6/BN6BattleProvider.java
@@ -8,6 +8,38 @@ import rand.DataProvider;
 import rand.RandomizerContext;
 
 public class BN6BattleProvider extends DataProvider<Battle> {
+	private static final byte[][] ENEMY_LEVELS = {
+		{ 1, 2, 3, 4, 5, 6 }, // Mettaur
+		{ 1, 2, 3, 4, 5, 6 }, // Piranha
+		{ 1, 2, 3, 4, 5, 6 }, // Heady
+		{ 1, 2, 3, 4, 5, 6 }, // Swordy
+		{ 1, 2, 3, 4, 5, 6 }, // KillerEye
+		{ 1, 2, 3, 4, 5, 6 }, // Quaker
+		{ 1, 2, 3, 4, 5, 6 }, // Catack
+		{ 1, 2, 3, 4, 5, 6 }, // Champy
+		{ 1, 1, 2, 2, 5, 5 }, // WindBox
+		{ 1, 2, 3, 4, 5, 6 }, // Trumpy
+		{ 1, 2, 3, 4, 5, 6 }, // OldStove
+		{ 1, 2, 3, 4, 5, 6 }, // HauntedCandle
+		{-1, 1, 2, 3, 4, 5 }, // Kettle
+		{ 1, 2, 3, 4, 5, 6 }, // Puffy
+		{ 1, 2, 3, 4, 5, 6 }, // StarFish
+		{ 1, 2, 3, 4, 5, 6 }, // EarthDragon
+		{ 1, 2, 3, 4, 5, 6 }, // ScareCrow
+		{ 1, 2, 3, 4, 5, 6 }, // PulseBulb
+		{ 1, 2, 3, 4, 5, 6 }, // BigHat
+		{ 1, 2, 3, 4, 5, 6 }, // BombCorn
+		{ 1, 2, 3, 4, 5, 6 }, // Shrubby
+		{ 1, 2, 3, 4, 5, 6 }, // HoneyBomber
+		{ 1, 2, 3, 4, 5, 6 }, // Gunner
+		{ 1, 2, 3, 4, 5, 6 }, // FighterPlane
+		{ 1, 2, 3, 4, 5, 6 }, // DarkMech
+		{ 1, 2, 3, 4, 5, 6 }, // SnakeArm
+		{ 1, 2, 3, 4, 5, 6 }, // Armadill
+		{ 1, 2, 3, 4, 5, 6 }, // Cragger
+		{ 1, 2, 3, 4, 5, 6 }, // Nightmare
+	};
+	
 	public BN6BattleProvider(RandomizerContext context, BattleProducer producer) {
 		super(context, producer);
 	}
@@ -24,12 +56,30 @@ public class BN6BattleProvider extends DataProvider<Battle> {
 				if (objValue >= 1 && objValue <= 0xAE) {
 					int enemyFamily = (objValue - 1) / 6;
 					int enemyRank = (objValue - 1) % 6;
-
-					// Do not randomize Rare viruses.
-					if (enemyRank <= 3) {
-						// Choose a random family but keep the same rank.
-						enemyFamily = rng.nextInt(29);
+					enemyRank = Math.abs(ENEMY_LEVELS[enemyFamily][enemyRank]);
+					
+					// Choose a random family.
+					enemyFamily = rng.nextInt(29);
+					
+					// Choose a random rank of the same difficulty.
+					byte[] possibleRanks = new byte[6];
+					int possibleCount = 0;
+					do {
+						for (byte j = 0; j < 6; j++) {
+							byte possibleRank = ENEMY_LEVELS[enemyFamily][j];
+							if (possibleRank == enemyRank) {
+								possibleRanks[possibleCount++] = j;
+							}
+						}
+						// Reduce enemy rank if not found.
+					} while (possibleCount == 0 && --enemyRank >= 1);
+					if (possibleCount == 0) {
+						// Should never happen!!!
+						continue;
 					}
+					
+					// Choose enemy level.
+					enemyRank = possibleRanks[rng.nextInt(possibleCount)];
 
 					objValue = enemyFamily * 6 + enemyRank + 1;
 				}
